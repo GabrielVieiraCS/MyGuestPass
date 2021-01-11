64f16fc0c582b83533bd69b5ea785b4b0ff410f7
Gabriel Vieira <60400576+GabrielVieiraCS@users.noreply.github.com>
2021-01-11 14:07:44 +0000

    LoginActivity + Animations

---
+++ mynfcapp/app/src/main/java/com/example/mynfcapp/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.mynfcapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+    }
+}
